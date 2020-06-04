f0476f4377b70b72a46d73987c44b8d6256b5539
strausr <Raya@cloudinary.com>
2020-06-04 11:20:19 +0300

    Feature/add action syntax resize.crop (#10)

--- __TESTS__/unit/actions/Resize.test.ts
+++ __TESTS__/unit/actions/Resize.test.ts
@@ -1,4 +1,4 @@
-import Resize, {minimumPad} from 'actions/resize/Resize';
+import Resize, {minimumPad, crop} from 'actions/resize/Resize';
 import TransformableImage from 'transformation/TransformableImage';
 
 describe('Tests for Transformation Action -- Resize', () => {
@@ -11,6 +11,20 @@ describe('Tests for Transformation Action -- Resize', () => {
     // Ensures it compiles and doesn't throw
     expect(
       tImage.resize(minimumPad(250, 250))
+    ).toEqual(tImage);
+  });
+});
+
+describe('Tests for Transformation Action -- Crop', () => {
+  it('Ensures "crop" is exported in the Resize namespace', () => {
+    expect(Resize.crop).toEqual(crop);
+  });
+
+  it('Ensures crop Is accepted as an action to TransformableImage', () => {
+    const tImage = new TransformableImage();
+    // Ensures it compiles and doesn't throw
+    expect(
+      tImage.resize(crop(250, 250))
     ).toEqual(tImage);
   });
 });
