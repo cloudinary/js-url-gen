591445e3cf817cbb51ae52b2fef79a7ed1026699
strausr <Raya@cloudinary.com>
2020-06-04 11:28:33 +0300

    Added Action Resize.fill (#12)

--- __TESTS__/unit/actions/Resize.test.ts
+++ __TESTS__/unit/actions/Resize.test.ts
@@ -1,5 +1,6 @@
 import Resize, {minimumPad, crop} from 'actions/resize/Resize';
 import TransformableImage from 'transformation/TransformableImage';
+import {fill} from "../../../src/actions/resize/Resize";
 
 describe('Tests for Transformation Action -- Resize', () => {
   it('Ensures "minimumPad" is exported in the Resize namespace', () => {
@@ -25,6 +26,20 @@ describe('Tests for Transformation Action -- Crop', () => {
     // Ensures it compiles and doesn't throw
     expect(
       tImage.resize(crop(250, 250))
+    ).toEqual(tImage);
+  });
+});
+
+describe('Tests for Transformation Action -- Fill', () => {
+  it('Ensures "fill" is exported in the Resize namespace', () => {
+    expect(Resize.fill).toEqual(fill);
+  });
+
+  it('Ensures fill Is accepted as an action to TransformableImage', () => {
+    const tImage = new TransformableImage();
+    // Ensures it compiles and doesn't throw
+    expect(
+      tImage.resize(fill(250, 250))
     ).toEqual(tImage);
   });
 });
