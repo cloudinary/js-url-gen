6331681b50c43ed1b01f79ce8895c7992dc31f5e
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-06-07 16:37:11 +0300

    Feature/add compilation tests (#14)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Transformations
 import Transformation from "./transformation/Transformation";
 import cloudinaryURL from "./url/cloudinaryURL";
-
 import TransformableImage from "./transformation/TransformableImage";
+
 // Actions
 import Actions from './actions/Actions';
 
@@ -14,5 +14,5 @@ class Base {
   }
 }
 
-export {cloudinaryURL, Transformation, Base, Actions};
+export {cloudinaryURL, Transformation, TransformableImage, Base, Actions};
 export default {cloudinaryURL, Transformation, Base, Actions};
