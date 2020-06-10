2a2d0945c01c73c8d8337c06942e6dd6d8d72ee9
Patrick <patrick.tolosa@cloudinary.com>
2020-06-10 18:49:28 +0300

    Add Constants file

---
+++ src/constants.ts
@@ -0,0 +1,24 @@
+export const ALLOWED_ACCOUNT_CONFIG = ['provisioning_api_key', 'provisioning_api_secret', 'account_id'];
+export const ALLOWED_URL_CONFIG = [
+  'force_version',
+  'shorten',
+  'sign_url',
+  'long_url_signature',
+  'use_root_path',
+  'auth_token'
+];
+export const ALLOWED_CLOUD_CONFIG = [
+  'cloud_name',
+  'secure_distribution',
+  'api_key',
+  'api_secret',
+  'private_cdn',
+  'cdn_subdomain',
+  'secure_cdn_subdomain',
+  'cname',
+  'ssl_detected',
+  'secure'
+];
+
+
+export const INVALID_TYPE_MESSAGE = 'Invalid Type or key received';
