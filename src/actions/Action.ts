962a3dd03a232a1dd29c15d02462c805c1f01b94
maoznir <nir.maoz@cloudinary.com>
2020-06-30 17:54:18 +0300

    Add Param, Flag, ParamValue

--- src/actions/Action.ts
+++ src/actions/Action.ts
@@ -1,6 +1,31 @@
-class Action {
+import Param from "../parameters/Param";
+import Flag from "../parameters/Flag";
+import {IAction} from "../interfaces/IAction";
+
+class Action implements IAction {
+  // We're using map, to overwrite existing keys. for example:
+  // addParam(w_100).addParam(w_200) should result in w_200. and not w_100,w_200
+  params: Map<string, Param> = new Map();
+  delimiter: string = ',' // {param}{delimiter}{param} for example: `${'w_100'}${','}${'c_fill'}`
+
   toString() {
-    return 'NOT_IMPLEMENTED';
+    return Array.from(this.params.values()).join(this.delimiter);
+  }
+
+  addParam(parameter: Param) {
+    this.params.set(parameter.key, parameter);
+    return this;
+  }
+
+  addFlag(flag: Flag) {
+    const existingFlag = this.params.get('fl_');
+    if (existingFlag){
+      existingFlag.addValue(flag.paramValue);
+    } else{
+      this.params.set('fl_', flag);
+    }
+
+    return this;
   }
 }
 
---
+++ src/parameters/ParamValue.ts
@@ -0,0 +1,20 @@
+class ParamValue {
+  values: any[] = [];
+  delimiter: string = '.'; // {value}{delimiter}{value}...
+
+  toString() {
+    return this.values.join(this.delimiter);
+  }
+
+  addValue(value: unknown) {
+    if (Array.isArray(value)) {
+      this.values = this.values.concat(value);
+    } else {
+      this.values.push(value);
+    }
+
+    return this;
+  }
+}
+
+export default ParamValue;
