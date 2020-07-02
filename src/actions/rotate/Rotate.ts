f32b1d59fd7588574c742b70ed44a72bc547f0d4
maoznir <nir.maoz@cloudinary.com>
2020-07-02 14:53:36 +0300

    Refactor Rotate action

--- src/actions/rotate/Rotate.ts
+++ src/actions/rotate/Rotate.ts
@@ -4,35 +4,40 @@ import angle from "./angle";
 import Action from "../Action";
 import {IAction} from "../../interfaces/IAction";
 import Param from "../../parameters/Param";
+const HORIZONTAL_FLIP = 'hflip';
+const VERTICAL_FLIP = 'vflip';
+const PARAM_KEY = 'a';
 
 class Rotate extends Action implements IAction {
   constructor(angle?: number) {
     super();
-    this.addParam(new Param('a', angle));
+    this.addParam(new Param(PARAM_KEY, angle));
   }
 
   /**
    * @return {Rotate}
    */
   horizontalFlip(): Rotate {
-    this.params.get('a').addValue('hflip');
-    return this;
+    return this.addValue(HORIZONTAL_FLIP);
   }
 
   /**
    * @return {Rotate}
    */
   verticalFlip(): Rotate {
-    this.params.get('a').addValue('vflip');
-    return this;
+    return this.addValue(VERTICAL_FLIP);
   }
 
   /**
    * @return {Rotate}
    */
   angle(value: number): Rotate {
-    if (typeof value !== 'undefined') {
-      this.params.get('a').addValue(value);
+    return this.addValue(value);
+  }
+
+  private addValue(value?: number | string): Rotate {
+    if (value != null) {
+      this.params.get(PARAM_KEY).addValue(value);
     }
     return this;
   }
