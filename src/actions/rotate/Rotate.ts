f63ea41fada2eea9eb816d3781f6dac5c9e82caa
Patrick <patrick.tolosa@cloudinary.com>
2020-07-02 18:26:08 +0300

    Refactor how we extend the paramValue in rotate

--- src/actions/rotate/Rotate.ts
+++ src/actions/rotate/Rotate.ts
@@ -4,41 +4,40 @@ import angle from "./angle";
 import Action from "../Action";
 import {IAction} from "../../interfaces/IAction";
 import Param from "../../parameters/Param";
+import ParamValue from "../../parameters/ParamValue";
 const HORIZONTAL_FLIP = 'hflip';
 const VERTICAL_FLIP = 'vflip';
-const PARAM_KEY = 'a';
 
 class Rotate extends Action implements IAction {
+  readonly angleParamValue: ParamValue ;
+
   constructor(angle?: number) {
     super();
-    this.addParam(new Param(PARAM_KEY, angle));
+    this.angleParamValue = new ParamValue(angle);
+    this.addParam(new Param('a', this.angleParamValue));
   }
 
   /**
    * @return {Rotate}
    */
   horizontalFlip(): Rotate {
-    return this.addValue(HORIZONTAL_FLIP);
+    this.angleParamValue.addValue(HORIZONTAL_FLIP);
+    return this;
   }
 
   /**
    * @return {Rotate}
    */
   verticalFlip(): Rotate {
-    return this.addValue(VERTICAL_FLIP);
+    this.angleParamValue.addValue(VERTICAL_FLIP);
+    return this;
   }
 
   /**
    * @return {Rotate}
    */
   angle(value: number): Rotate {
-    return this.addValue(value);
-  }
-
-  private addValue(value?: number | string): Rotate {
-    if (value != null) {
-      this.params.get(PARAM_KEY).addValue(value);
-    }
+    this.angleParamValue.addValue(value);
     return this;
   }
 }
