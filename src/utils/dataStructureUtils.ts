83a3720ad3a5034e3699298a8c9f7afd04ca6438
maoznir <nir.maoz@cloudinary.com>
2020-07-02 15:54:44 +0300

    Add sortMapByKey docString

--- src/utils/dataStructureUtils.ts
+++ src/utils/dataStructureUtils.ts
@@ -1,5 +1,10 @@
 import {IKeyObject} from "../interfaces/IKeyObject";
 
+/**
+ * Sort a map of IKeyObject by key
+ * @param {<string, IKeyObject>)} map to sort
+ * @Return sorted map
+ */
 function sortMapByKey(map: Map<string, IKeyObject>) {
   return (Array.from(map.values()) as IKeyObject[]).sort((obj1, obj2) => {
     // No need to check for equality because this is a map.
