a61344a25350ea6115e2f768e9cecb136d87183a
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-07-16 16:53:04 +0300

    Implement parts of adjust (#47)

--- src/actions/Actions.ts
+++ src/actions/Actions.ts
@@ -5,6 +5,7 @@ import Effect from "./effect/Effect";
 import Quality from "./quality/Quality";
 import Rotate from "./rotate/Rotate";
 import Color from "./color/Color";
+import Adjust from "./adjust/Adjust";
 
-export {Resize, Border, RoundCorners, Effect, Quality, Rotate, Color};
-export default {Resize, Border, RoundCorners, Effect, Quality, Rotate, Color};
+export {Resize, Border, RoundCorners, Effect, Quality, Rotate, Color, Adjust};
+export default {Resize, Border, RoundCorners, Effect, Quality, Rotate, Color, Adjust};
