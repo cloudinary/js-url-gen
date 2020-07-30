25b48b1b998cce179cf0b002334f644533b2f004
Patrick <patrick.tolosa@cloudinary.com>
2020-07-30 15:36:14 +0300

    Add missing actions

--- src/actions/Actions.ts
+++ src/actions/Actions.ts
@@ -6,6 +6,8 @@ import Quality from "./quality/Quality";
 import Rotate from "./rotate/Rotate";
 import Adjust from "./adjust/Adjust";
 import Layers from "./layers/Layers";
+import Variable from "./variable/Variable";
+import NamedTransformation from './namedTransformation/NamedTransformation';
 
-export {Resize, Border, RoundCorners, Effect, Quality, Rotate, Adjust, Layers};
-export default {Resize, Border, RoundCorners, Effect, Quality, Rotate, Adjust, Layers};
+export {Resize, Border, RoundCorners, Effect, Quality, Rotate, Adjust, Layers, Variable, NamedTransformation};
+export default {Resize, Border, RoundCorners, Effect, Quality, Rotate, Adjust, Layers, Variable, NamedTransformation};
