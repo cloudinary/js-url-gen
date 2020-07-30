0f62ca85883b89124def2dec1316c98196bfe162
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-07-30 16:10:12 +0300

    Validate the contents of /dist when we create entrypoints (#51)
    * Validate the contents of /dist when we create entry-points

---
+++ __TESTS__/unit/scripts/validatePackageFiles.test.ts
@@ -0,0 +1,76 @@
+/* eslint @typescript-eslint/no-var-requires: 0 */
+
+const validatePackageFiles = require('../../../scripts/validatePackageFiles');
+const MissingRequiredFile = require('../../../scripts/errors/MissingRequiredFile');
+const FoundNonRequiredFile= require('../../../scripts/errors/FoundNonRequiredFile');
+
+global.console = console;
+const mock = require('mock-fs');
+
+
+describe('Tests for ValidatePackageFiles', () => {
+  beforeEach(() => {
+    mock.restore();
+    mock({
+      'dist': {
+        foo: '//AnyFileContents',
+        bar: {
+          foobar: '//AnyFileContents'
+        }
+      }
+    });
+  });
+
+  it('Validates an input JSON with a mocked file system', () => {
+    validatePackageFiles({
+      foo: 'file',
+      bar: {
+        foobar: 'file'
+      }
+    });
+  });
+
+  it('Will throw if a file needs to exist, but does not exist', () => {
+    expect(() => {
+      validatePackageFiles({
+        missingFoo: 'file',
+        foo: 'file',
+        bar: {
+          foobar: 'file'
+        }
+      }, './dist');
+    }).toThrow(new MissingRequiredFile('dist/missingFoo'));
+  });
+
+
+  it('Will throw if the file system has more entries than the JSON', () => {
+    expect(() => {
+      validatePackageFiles({
+        foo: 'file',
+        bar: {}
+      }, './dist');
+    }).toThrow(new FoundNonRequiredFile('dist/bar/foobar'));
+  });
+
+  it('Will not throw when a key is explicitly set to ignore ', () => {
+    validatePackageFiles({
+      foo: 'file',
+      bar: false
+    }, './dist');
+  });
+
+  it('Will not throw when a a flag is passed and there are missing files in tree', () => {
+    validatePackageFiles({
+      $suppressNonRequiredFilesError: true,
+      foo: 'file'
+    }, './dist');
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+});
+
+
+
+
