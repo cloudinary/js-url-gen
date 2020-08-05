42cf0c8360a21a477a987fa1b53367aeb4a47973
Patrick <patrick.tolosa@cloudinary.com>
2020-08-05 10:32:29 +0300

    Add string types to resize

--- src/actions/resize/IResizeAction.ts
+++ src/actions/resize/IResizeAction.ts
@@ -2,8 +2,8 @@ import {IAction} from "../../interfaces/IAction";
 import {GravityParam} from "../../params/gravity/Gravity";
 
 export interface IResizeAction extends IAction{
-  aspectRatio(ratio:string | number):IResizeAction;
+  aspectRatio(ratio:number|string):IResizeAction;
   gravity(type:GravityParam):IResizeAction;
-  height(x:number):IResizeAction;
-  width(x:number):IResizeAction;
+  height(x:number|string):IResizeAction;
+  width(x:number|string):IResizeAction;
 }
