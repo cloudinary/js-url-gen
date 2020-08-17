0b1fac76913a4f6ac539eda8115bfa0085ced3c4
Raya Straus <raya@cloudinary.com>
2020-08-17 11:29:40 +0300

    updated tests

--- __TESTS__/unit/actions/Condition.test.ts
+++ __TESTS__/unit/actions/Condition.test.ts
@@ -2,6 +2,7 @@ import Condition, {fromString} from '../../../src/actions/condition/Condition';
 import CloudinaryConfig from "../../../src/config/CloudinaryConfig";
 import TransformableImage from "../../../src/transformation/TransformableImage";
 import {variable} from "../../../src/actions/variable/Variable";
+import Resize from "../../../src/actions/resize/Resize";
 
 const CONFIG_INSTANCE = new CloudinaryConfig({
   cloud: {
@@ -22,60 +23,66 @@ describe('Tests for Transformation Action -- Condition', () => {
     const url = new TransformableImage()
       .setConfig(CONFIG_INSTANCE)
       .ifCondition(Condition.fromString('ar = 1.0'))
+      .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_eq_1.0/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_eq_1.0/if_end/sample');
   });
 
   it('Creates a cloudinaryURL with condition !=', () => {
     const url = new TransformableImage()
       .setConfig(CONFIG_INSTANCE)
       .ifCondition(Condition.fromString('ar != 1.0'))
+      .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_ne_1.0/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_ne_1.0/if_end/sample');
   });
 
   it('Creates a cloudinaryURL with condition <', () => {
     const url = new TransformableImage()
       .setConfig(CONFIG_INSTANCE)
       .ifCondition(Condition.fromString('ar < 1.0'))
+      .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_lt_1.0/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_lt_1.0/if_end/sample');
   });
 
   it('Creates a cloudinaryURL with condition >', () => {
     const url = new TransformableImage()
       .setConfig(CONFIG_INSTANCE)
       .ifCondition(Condition.fromString('ar > 1.0'))
+      .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_gt_1.0/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_gt_1.0/if_end/sample');
   });
 
   it('Creates a cloudinaryURL with condition <=', () => {
     const url = new TransformableImage()
       .setConfig(CONFIG_INSTANCE)
       .ifCondition(Condition.fromString('ar <= 1.0'))
+      .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_lte_1.0/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_lte_1.0/if_end/sample');
   });
 
   it('Creates a cloudinaryURL with condition >=', () => {
     const url = new TransformableImage()
       .setConfig(CONFIG_INSTANCE)
       .ifCondition(Condition.fromString('ar >= 1.0'))
+      .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_gte_1.0/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_ar_gte_1.0/if_end/sample');
   });
 
   it('Creates a cloudinaryURL with condition ar > 0.65 and w > 1000', () => {
@@ -83,35 +90,25 @@ describe('Tests for Transformation Action -- Condition', () => {
       .setConfig(CONFIG_INSTANCE)
       .variable(variable('a', 30))
       .ifCondition(Condition.fromString('ar > 0.65 and w > 1000'))
+      .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/$a_30/if_ar_gt_0.65_and_w_gt_1000/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/$a_30/if_ar_gt_0.65_and_w_gt_1000/if_end/sample');
   });
 
   it('Creates a cloudinaryURL with elseIf', () => {
     const url = new TransformableImage()
       .setConfig(CONFIG_INSTANCE)
       .ifCondition(Condition.fromString('w <= 500'))
+      .resize(Resize.crop(400))
       .ifElse()
-      .variable(variable('a', 30))
-      .setPublicID('sample')
-      .toURL();
-
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_w_lte_500/if_else/$a_30/sample');
-  });
-
-  it('Creates a cloudinaryURL with elseIf and endIfCondition', () => {
-    const url = new TransformableImage()
-      .setConfig(CONFIG_INSTANCE)
-      .ifCondition(Condition.fromString('w <= 500'))
-      .ifElse()
-      .variable(variable('a', 30))
+      .resize(Resize.crop(500))
       .endIfCondition()
       .setPublicID('sample')
       .toURL();
 
-    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_w_lte_500/if_else/$a_30/if_end/sample');
+    expect(url).toBe('http://res.cloudinary.com/demo/image/upload/if_w_lte_500/c_crop,w_400/if_else/c_crop,w_500/if_end/sample');
   });
 
 });
