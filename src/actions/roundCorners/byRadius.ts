147e34368876a2613e3e42a8a988267b110df946
Patrick <patrick.tolosa@cloudinary.com>
2020-08-17 11:50:08 +0300

    Adjust JSdoc strings

--- src/actions/roundCorners/byRadius.ts
+++ src/actions/roundCorners/byRadius.ts
@@ -3,9 +3,16 @@ import RoundCornersAction from "./RoundCornersAction";
 
 /**
  * @memberOf Actions.RoundCorners
- * @description Rounds the specified corners of an image by specifying 1-4 pixel values as follows:
+ * @description Rounds the specified corners of an image or a video by specifying 1-4 pixel values as follows:
+ *
+ * **Learn more**:
+ * <a href=https://cloudinary.com/documentation/image_transformations#rounding_corners_and_creating_circular_images
+ * target="_blank">Rounded images</a> |
+ * <a href=
+ * https://cloudinary.com/documentation/video_manipulation_and_delivery#rounding_corners_and_creating_circular_videos
+ * target="_blank">Rounded videos</a>
  * @param {number} a
- * @param {number} [b] ;
+ * @param {number} [b];
  * @param {number} [c];
  * @param {number} [d];
  * @return {RoundCornersAction}
