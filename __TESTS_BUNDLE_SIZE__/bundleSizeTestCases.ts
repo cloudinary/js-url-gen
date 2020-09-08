2811f1b908959c66b396661d2fcddf05cecdb382
Raya Straus <raya@cloudinary.com>
2020-09-08 10:52:01 +0300

    updated bundle size

--- __TESTS_BUNDLE_SIZE__/bundleSizeTestCases.ts
+++ __TESTS_BUNDLE_SIZE__/bundleSizeTestCases.ts
@@ -42,7 +42,7 @@ const bundleSizeTestCases:ITestCase[] = [
   },
   {
     name: 'Tests a transformable image with Resize, adjust and delivery',
-    sizeLimitInKB: 16,
+    sizeLimitInKB: 17,
     importsArray: [
       importFromBase('TransformableImage'),
       importFromDist('actions/resize'),
