846cc97699105fc958064f24530bd4c38f652c96
Patrick <patrick.tolosa@cloudinary.com>
2020-09-14 15:35:40 +0300

    Align cartoonify

--- src/actions/effect/cartoonify.ts
+++ src/actions/effect/cartoonify.ts
@@ -1,13 +1,41 @@
-import SimpleEffectAction from "./EffectActions/SimpleEffectAction";
+import Param from "../../parameters/Param";
+import ParamValue from "../../parameters/ParamValue";
+import Action from "../Action";
 
+class CartoonifyEffect extends Action {
+  private colorReduction: number | string;
+  private cartoonifyStrength: number;
+  private effectName: string;
+  constructor(effectName:string, strength:number) {
+    // We don't pass level in the constructor, we'll build it in the prepareParam
+    super();
+    this.cartoonifyStrength = strength;
+    this.effectName = effectName;
+  }
+
+  strength(cartoonifyLevel: number) {
+    this.cartoonifyStrength = cartoonifyLevel;
+    return this;
+  }
+
+  colorReductionLevel(level: number | string) {
+    this.colorReduction = level;
+    return this;
+  }
+
+  protected prepareParam() {
+    this.addParam(new Param('e', new ParamValue([this.effectName, this.cartoonifyStrength, this.colorReduction])));
+    return;
+  }
+}
 
 /**
  * @description Applies a cartoonify filter to the asset.
  * @memberOf Actions.Effect
  * @param cartoonifyLevel
  */
-function cartoonify(cartoonifyLevel?: number):SimpleEffectAction {
-  return new SimpleEffectAction('cartoonify', cartoonifyLevel);
+function cartoonify(cartoonifyLevel?: number):CartoonifyEffect {
+  return new CartoonifyEffect('cartoonify', cartoonifyLevel);
 }
 
 export default cartoonify;
