1cac96f3f250e1d4fda978c479c275893dfa2cb0
Patrick <patrick.tolosa@cloudinary.com>
2020-09-17 13:01:36 +0300

    Align viesus correct

--- src/actions/adjust/simple/viesusCorrect.ts
+++ src/actions/adjust/simple/viesusCorrect.ts
@@ -1,13 +1,55 @@
-import SimpleEffectAction from "../../effect/EffectActions/SimpleEffectAction";
+import Action from "../../Action";
+import Qualifier from "../../../qualifiers/Qualifier";
+
+
+class ViesusCorrectAdjustAction extends Action {
+  private _noRedeye : boolean;
+  private _skinSaturation: boolean;
+  private _skinSaturationLevel: number;
+
+  noRedeye() {
+    this._noRedeye = true;
+    return this;
+  }
+
+  /**
+   * @description The enhancement level. (Range: -100 to 100, Server default: 50).
+   * @param level
+   */
+  skinSaturation(level?:number) {
+    this._skinSaturation = true;
+    if (level) {
+      this._skinSaturationLevel = level;
+    }
+    return this;
+  }
+
+  protected prepareQualifiers() {
+    let value = 'viesus_correct';
+
+    if (this._noRedeye) {
+      value += ':no_redeye';
+    }
+
+    if (this._skinSaturation) {
+      value += ':skin_saturation';
+      if (typeof this._skinSaturationLevel !== 'undefined') {
+        value += `:${this._skinSaturationLevel}`;
+      }
+    }
+
+    this.addQualifier(new Qualifier('e', value));
+  }
+}
 
 /**
  * @memberOf Actions.Adjust
- * @description Enhances an image to its best visual quality with the Viesus Automatic Image Enhancement add-on.
- * For details, see the Viesus Automatic Image Enhancement add-on documentation.
-
+ * @description
+ * Enhances an image to its best visual quality with the Viesus Automatic Image Enhancement add-on.</br>
+ * <b>Learn more:</b> {@link https://cloudinary.com/documentation/viesus_automatic_image_enhancement_addon | Viesus Automatic Image Enhancement.}
  */
-function viesusCorrect(): SimpleEffectAction {
-  return new SimpleEffectAction('viesus_correct');
+function viesusCorrect(): ViesusCorrectAdjustAction {
+  return new ViesusCorrectAdjustAction();
 }
 
 export default viesusCorrect;
