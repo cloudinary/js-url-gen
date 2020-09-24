de4d928839e447f3caec687da57df86c83ce25a6
Patrick <patrick.tolosa@cloudinary.com>
2020-09-24 13:08:34 +0300

    Add docs for EffectActionWithLevel

--- src/actions/effect/EffectActions/EffectActionWithLevel.ts
+++ src/actions/effect/EffectActions/EffectActionWithLevel.ts
@@ -1,5 +1,10 @@
 import LeveledEffectAction from "./LeveledEffectAction";
 
+/**
+ * @class EffectActionWithLevel
+ * @augments LeveledEffectAction
+ * @description A class for all effects that include a level method
+ */
 class EffectActionWithLevel extends LeveledEffectAction {
   level(value: number | string): this {
     return this.setLevel(value);
