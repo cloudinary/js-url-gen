864cf04eb0de55bc7477e1008edf06383d86e944
Patrick <patrick.tolosa@cloudinary.com>
2020-10-01 10:26:14 +0300

    Add @description to jsdoc

--- src/actions/adjust/RecolorAction.ts
+++ src/actions/adjust/RecolorAction.ts
@@ -5,7 +5,7 @@ import QualifierValue from "../../qualifier/QualifierValue";
 /**
  * @class RecolorAction
  * @augments Action
- * Converts the colors of every pixel in an image based on the supplied color matrix, in which the value of each color channel is calculated based on the values from all other channels (e.g. a 3x3 matrix for RGB, a 4x4 matrix for RGBA or CMYK, etc).<br/>
+ * @description Converts the colors of every pixel in an image based on the supplied color matrix, in which the value of each color channel is calculated based on the values from all other channels (e.g. a 3x3 matrix for RGB, a 4x4 matrix for RGBA or CMYK, etc).<br/>
  * For every pixel in the image, take each color channel and adjust its value by the specified values of the matrix to get a new value.
  */
 class RecolorAction extends Action {
