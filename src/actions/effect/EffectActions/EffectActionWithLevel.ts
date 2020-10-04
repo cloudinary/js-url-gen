3ca4e5089d91e1856638428fdc3e10c5574b2a18
Patrick <patrick.tolosa@cloudinary.com>
2020-10-04 17:37:26 +0300

    Fix text

--- src/actions/effect/EffectActions/EffectActionWithLevel.ts
+++ src/actions/effect/EffectActions/EffectActionWithLevel.ts
@@ -3,7 +3,7 @@ import LeveledEffectAction from "./LeveledEffectAction";
 /**
  * @class EffectActionWithLevel
  * @augments LeveledEffectAction
- * @description A class that provides a built in level() method that sets that level of the effect
+ * @description A class that provides a built in level() method that sets the level of the effect
  */
 class EffectActionWithLevel extends LeveledEffectAction {
   level(value: number | string): this {
