3995974cae5229b0d4cb437994ff453f813d67a4
Raya Straus <raya@cloudinary.com>
2020-10-15 11:46:22 +0300

    updated cutter action

--- src/actions/cutter/CutterAction.ts
+++ src/actions/cutter/CutterAction.ts
@@ -3,7 +3,14 @@ import {ImageSource} from "../../values/sources/sourceTypes/ImageSource";
 import {imageLayer} from "../overlay/Overlay";
 import {Flag} from "../../values/flag/Flag";
 import {LayerAction} from "../overlay/LayerAction";
+import {PositionQualifier} from "../../values/position/PositionQualifier";
 
+/**
+ * @@doc
+ * @description A class for Cutter
+ * @class CutterAction
+ * @augments Action
+ */
 class CutterAction extends Action {
   private layer: LayerAction;
   constructor(source: ImageSource) {
@@ -12,6 +19,15 @@ class CutterAction extends Action {
     // `modifications` is the action used as the last component of a layer
     // we can leverage that to add more things to it
     this.layer.modifications.addFlag(new Flag('cutter'));
+  }
+
+  /**
+   * @description Defines the position of the layer.
+   * @param {Values.Position} position The position of the overlay with respect to the base image.
+   */
+  position(position?:PositionQualifier): this{
+    this.layer.position = position;
+    return this;
   }
 
   toString(): string {
