a5fc365f52f71f906d2006d9534f0653593c9bf5
Patrick <patrick.tolosa@cloudinary.com>
2020-10-21 23:34:21 +0300

    Export cloudinary instance as the default

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import Transformation from "./transformation/Transformation";
 import cloudinaryURL from "./url/cloudinaryURL";
 import TransformableImage from "./transformation/TransformableImage";
 import Actions from './actions/Actions';
+import Cloudinary from "./instance/Cloudinary";
 
 
-export {cloudinaryURL, Transformation, TransformableImage, Actions};
-export default {cloudinaryURL, Transformation, Actions};
+export {cloudinaryURL, Transformation, TransformableImage, Actions, Cloudinary};
+export default Cloudinary;
+
