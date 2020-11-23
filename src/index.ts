5cb7cb3e6c60f204c5c0ad926793c5734bbd81fe
Patrick <patrick.tolosa@cloudinary.com>
2020-11-23 12:02:03 +0200

    Add TransformableVideo to the root exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import Transformation from "./transformation/Transformation";
 import cloudinaryURL from "./url/cloudinaryURL";
 import TransformableImage from "./transformation/TransformableImage";
+import TransformableVideo from "./transformation/TransformableVideo";
 import * as Actions from './actions/Actions';
 import Cloudinary from "./instance/Cloudinary";
 
-export {cloudinaryURL, Transformation, TransformableImage, Actions, Cloudinary};
+export {
+  cloudinaryURL,
+  Transformation,
+  TransformableImage,
+  TransformableVideo,
+  Actions,
+  Cloudinary
+};
 
