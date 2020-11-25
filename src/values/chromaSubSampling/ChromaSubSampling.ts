9cf91a4dca07eaaae3e83b4afc78d3e144fb1c0a
Patrick <patrick.tolosa@cloudinary.com>
2020-11-25 08:32:42 +0200

    Alight Chroma sub sampling

--- src/values/chromaSubSampling/ChromaSubSampling.ts
+++ src/values/chromaSubSampling/ChromaSubSampling.ts
@@ -2,21 +2,24 @@
  * @memberOf Values
  * @namespace ChromeSubSampling
  */
+class ChromaSubSampling {
+  /**
+   * @@doc
+   * @memberOf Values.ChromeSubSampling
+   */
+  static chroma444():number { return 444; }
 
+  /**
+   * * @@doc
+   * @memberOf Values.ChromeSubSampling
+   */
+  static chroma420():number { return 420; }
+}
 
-/**
- * @@doc
- * @memberOf Values.ChromeSubSampling
- */
-function chroma444():number { return 444; }
-
-/**
- * * @@doc
- * @memberOf Values.ChromeSubSampling
- */
-function chroma420():number { return 420; }
+const {chroma420, chroma444} = ChromaSubSampling
 
 export {
+  ChromaSubSampling,
   chroma420,
   chroma444
 };
