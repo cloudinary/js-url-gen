2c92ab7438a2db7e7cbffa3efacbd8964b1beda4
Patrick <patrick.tolosa@cloudinary.com>
2020-12-02 12:09:14 +0200

    Align background-color

--- src/actions/background/Background.ts
+++
@@ -1,9 +0,0 @@
-import {color} from "./bgColor";
-
-/**
- * @description - Contains functions that transform the background of transparent images or videos, or set the color of the padding used with 'pad' crop modes.
- * @memberOf Actions
- * @namespace Background
- */
-
-export {color};
