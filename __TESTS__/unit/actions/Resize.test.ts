dcf172ecd6427cef075953562d9759bbcb7af634
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-12-02 10:44:20 +0200

    remove resizeMode from Resize (#225)

--- __TESTS__/unit/actions/Resize.test.ts
+++ __TESTS__/unit/actions/Resize.test.ts
@@ -1,19 +1,16 @@
-import * as ResizeMode from "../../../src/values/resizeMode/ResizeMode";
 import getImageWithResize from "./Resize/shared/getImageWithResize";
 import * as Resize from "../../../src/actions/resize/Resize";
-import * as ResizeESM from "../../../src/actions/resize/Resize";
 
 
 describe('Tests for Transformation Action -- Resize', () => {
   it('Resize with a resize mode - relative', () => {
-    // TODO this is unspecced, but will probably be implemented like this.
-    const url = getImageWithResize(Resize.scale(100).resizeMode(ResizeMode.relative()), 'url');
+    const url = getImageWithResize(Resize.scale(100).relative(), 'url');
     expect(url).toContain('c_scale,fl_relative,w_100');
   });
 
   it('Resize with a resize mode - region_relative', () => {
     // TODO this is unspecced, but will probably be implemented like this.
-    const url = getImageWithResize(Resize.scale(100).resizeMode(ResizeMode.regionRelative()), 'url');
+    const url = getImageWithResize(Resize.scale(100).regionRelative(), 'url');
     expect(url).toContain('c_scale,fl_region_relative,w_100');
   });
 });
