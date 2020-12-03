ac3ac2328069cd6dc3683498619146f1efac7310
Patrick <patrick.tolosa@cloudinary.com>
2020-12-03 18:34:54 +0200

    Align exports for dither

--- src/values/dither/Dither.ts
+++ src/values/dither/Dither.ts
@@ -158,7 +158,7 @@ function circles7x7white(): number {
 }
 
 
-export {
+const Dither = {
   checkerboard2x1dither,
   circles5x5black,
   circles5x5white,
@@ -180,3 +180,29 @@ export {
   threshold1x1nondither
 };
 
+
+
+
+
+export {
+  Dither,
+  checkerboard2x1dither,
+  circles5x5black,
+  circles5x5white,
+  circles6x6black,
+  circles6x6white,
+  circles7x7black,
+  circles7x7white,
+  halftone4x4angled,
+  halftone4x4orthogonal,
+  halftone6x6angled,
+  halftone6x6orthogonal,
+  halftone8x8angled,
+  halftone8x8orthogonal,
+  halftone16x16orthogonal,
+  ordered2x2dispersed,
+  ordered3x3dispersed,
+  ordered4x4dispersed,
+  ordered8x8dispersed,
+  threshold1x1nondither
+};
