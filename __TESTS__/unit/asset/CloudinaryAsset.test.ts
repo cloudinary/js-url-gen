6bb19166355ba81238db517cf1afb89acf3fc176
Patrick <patrick.tolosa@cloudinary.com>
2020-12-09 14:49:15 +0200

    Add and remove tests as needed

---
+++ __TESTS__/unit/asset/CloudinaryAsset.test.ts
@@ -0,0 +1,46 @@
+import {CloudinaryAsset} from "../../../src/assets/CloudinaryAsset";
+import {Transformation} from "../../../src";
+
+
+describe('Tests for cloudinaryAsset', () => {
+  let cloudinaryAsset: CloudinaryAsset = null;
+  beforeEach(() => {
+    cloudinaryAsset = new CloudinaryAsset('sample', new Transformation());
+  });
+
+  it('Instantiates a cloudinaryAsset', () => {
+    expect(cloudinaryAsset).toBeDefined();
+  });
+
+  it('setPublicID should be chainable', () => {
+    expect(cloudinaryAsset.setPublicID('sample')).toBe(cloudinaryAsset);
+  });
+
+  it('Sign should be chainable', () => {
+    expect(cloudinaryAsset.sign()).toBe(cloudinaryAsset);
+  });
+
+  it('setConfig should be chainable', () => {
+    expect(cloudinaryAsset.setConfig({
+      cloud: {
+        cloudName: 'foo'
+      }
+    })).toBe(cloudinaryAsset);
+  });
+
+  it('Can use the asset descriptor methods without breaking (Internals not yet implemented)', () => {
+    expect(cloudinaryAsset
+      .setConfig({
+        cloud: {
+          cloudName: 'demo'
+        }
+      })
+      .setStorageType('fetch')
+      .setAssetType('avatar')
+      .setVersion(1234)
+      .setPublicID('sample')
+      .toURL()
+    ).toBe('https://res.cloudinary.com/demo/avatar/fetch/v1234/sample');
+  });
+});
+
--- src/values/sources/sourceTypes/VideoSource.ts
+++ src/values/sources/sourceTypes/VideoSource.ts
@@ -1,8 +1,8 @@
 import {ISource} from "../ISource";
-import TransformableVideo from "../../../transformation/TransformableVideo";
+import {CloudinaryVideo} from "../../../assets/CloudinaryVideo";
 
 /**
- * @description Defines how to manipulate a video layer, is an instance of a {@link TransformableVideo|TransformableVideo}
+ * @description Defines how to manipulate a video layer, is an instance of a {@link VideoTransformation|VideoTransformation}
  * <div class="panel panel-primary">
  *   <div class="panel-heading">Notice</div>
  *   <div class="panel-body">
@@ -10,21 +10,21 @@ import TransformableVideo from "../../../transformation/TransformableVideo";
  *     You can find regular images and videos transformations below:
  *   </div>
  *   <ul>
- *     <li>{@link TransformableImage| Image Transformations}</li>
- *     <li>{@link TransformableVideo| Video Transformations}
+ *     <li>{@link ImageTransformation| Image Transformations}</li>
+ *     <li>{@link VideoTransformation| Video Transformations}
  *   </ul>
  * </div>
  * @class VideoSource
- * @augments {TransformableVideo}
+ * @augments {VideoTransformation}
  * @implements {ISource}
  */
-class VideoSource extends TransformableVideo implements ISource {
+class VideoSource extends CloudinaryVideo implements ISource {
   getSource():string {
     return this.asset.publicID;
   }
 
   getTransformationString():string {
-    return this.toString();
+    return this.transformation.toString();
   }
 }
 
