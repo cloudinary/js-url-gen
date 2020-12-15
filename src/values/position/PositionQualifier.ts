7eaef6fbb465ecc04fdd7fe84d93725e3cb1ec14
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-12-15 10:26:50 +0200

    Align the Position qualifier (#250)

--- src/values/position/PositionQualifier.ts
+++ src/values/position/PositionQualifier.ts
@@ -1,6 +1,10 @@
 import {Action} from "../../internal/Action";
+import {CompassGravity} from "../gravity/compassGravity/CompassGravity";
+import {OCR} from "../gravity/OCR/OCR";
+import {FocusOnGravity} from "../gravity/focusOnGravity/FocusOnGravity";
 import {Qualifier} from "../../internal/qualifier/Qualifier";
-import {GravityQualifier} from "../gravity/GravityQualifier";
+import {tiled} from "../flag/tiled";
+import {noOverflow} from "../flag/noOverflow";
 
 /**
  * @description
@@ -11,12 +15,36 @@ import {GravityQualifier} from "../gravity/GravityQualifier";
  * <b>Learn more:</b> {@link https://cloudinary.com/documentation/image_transformations#image_and_text_overlays|Applying overlays to images} | {@link https://cloudinary.com/documentation/video_manipulation_and_delivery#adding_image_overlays|Applying overlays to videos}
  *
  * @class PositionQualifier
- * @augments Action
+ * @augments {SDK.Actions}
  */
 class PositionQualifier extends Action {
-  constructor(positionDirection:string) {
+  constructor() {
     super();
-    this.addQualifier(new GravityQualifier(positionDirection));
+  }
+
+  gravity(gravityQualifier: CompassGravity | FocusOnGravity | OCR): this {
+    this.addQualifier(gravityQualifier);
+    return this;
+  }
+
+  /**
+   * @description Tile the overlay across your image.
+   * @doc
+   * <b>Learn more:</b> {@link https://cloudinary.com/documentation/image_transformations#tiling_overlays | Tiling overlay}
+   */
+  tiled(): this {
+    this.addFlag(tiled());
+    return this;
+  }
+
+  /**
+   * @description Prevents an image or text overlay from extending a delivered image canvas beyond the dimensions of the base image
+   * @doc
+   * <b>Learn more:</b> {@link https://cloudinary.com/documentation/transformation_reference#fl_no_overflow | Overflow in overlays}
+   */
+  allowOverflow(): this {
+    this.addFlag(noOverflow());
+    return this;
   }
 
   /**
@@ -25,7 +53,8 @@ class PositionQualifier extends Action {
    * @return {this}
    */
   x(offsetX:number): this{
-    return this.addQualifier(new Qualifier('x', offsetX));
+    this.addQualifier(new Qualifier('x', offsetX));
+    return this;
   }
 
   /**
@@ -34,7 +63,8 @@ class PositionQualifier extends Action {
    * @return {this}
    */
   y(offsetY:number): this{
-    return this.addQualifier(new Qualifier('y', offsetY));
+    this.addQualifier(new Qualifier('y', offsetY));
+    return this;
   }
 }
 
