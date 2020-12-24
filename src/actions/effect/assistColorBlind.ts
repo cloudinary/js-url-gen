af461c63a28f4a098184d5f71f166464bdc9df65
Patrick <patrick.tolosa@cloudinary.com>
2020-12-24 11:47:33 +0200

    Align assist color blind to the spec

--- src/actions/effect/assistColorBlind.ts
+++ src/actions/effect/assistColorBlind.ts
@@ -6,10 +6,9 @@ import {Qualifier} from "../../internal/qualifier/Qualifier";
  * @augments Action
  * @description Applies stripes to the image to help people with common color-blind conditions to differentiate between colors that are similar for them.
  * You can replace colors using the xray() method.
+ * @memberOf Actions.Effect
  */
 class AssistColorBlindEffectAction extends Action {
-  private hasStripes:boolean;
-
   constructor() {
     super();
     this.addQualifier(new Qualifier('e', new QualifierValue('assist_colorblind')));
@@ -20,22 +19,16 @@ class AssistColorBlindEffectAction extends Action {
    * @return {this}
    */
   xray(): this{
-    // If stripes are defined, we don't do anything with xray
-    if (!this.hasStripes) {
-      return this.addQualifier(new Qualifier('e', new QualifierValue(['assist_colorblind', 'xray']).setDelimiter(':')));
-    } else {
-      return this;
-    }
+    return this.addQualifier(new Qualifier('e', new QualifierValue(['assist_colorblind', 'xray']).setDelimiter(':')));
   }
 
   /**
    * @description Applies stripes of the specified intensity to help people with common color blind conditions to differentiate between colors that are similar for them.
-   * @param {number | string} num The intensity of the stripes. (Range: 1 to 100, Server default: 10)
+   * @param {number | string} strength The intensity of the stripes. (Range: 1 to 100, Server default: 10)
    * @return {this}
    */
-  stripes(num:number | string): this {
-    this.hasStripes = true;
-    return this.addQualifier(new Qualifier('e', new QualifierValue(['assist_colorblind', num]).setDelimiter(':')));
+  stripesStrength(strength:number | string): this {
+    return this.addQualifier(new Qualifier('e', new QualifierValue(['assist_colorblind', strength]).setDelimiter(':')));
   }
 }
 
