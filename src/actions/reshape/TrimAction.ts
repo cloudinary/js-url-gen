e62a40d17edfff23df7bb26c0b611e4019b8018c
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2021-01-12 11:46:16 +0200

    Implement reshape.trim (#302)

---
+++ src/actions/reshape/TrimAction.ts
@@ -0,0 +1,43 @@
+import {Action} from "../../internal/Action";
+import {SystemColors} from "../../values/color";
+
+/**
+ * @description Removes the edges of the image based on the color of the corner pixels.
+ * Specify a color other than the color of the corner pixels using the colorOverride() method
+ * @memberOf Actions.Reshape
+ * @extends {SDK.Action}
+ */
+class TrimAction extends Action {
+  private _tolerance: number;
+  private _color: SystemColors | string;
+
+  /**
+   * @param {number} tolerance The tolerance level for color similarity.
+   */
+  colorSimilarity(tolerance: number): this {
+    this._tolerance = tolerance;
+    return this;
+  }
+
+  /**
+   * @param {string | Values.Color} color Overrides the corner pixels color with the specified color.
+   */
+  colorOverride(color: SystemColors | string): this {
+    this._color = color;
+    return this;
+  }
+
+  toString(): string {
+    // image.reshape(Reshape.trim()->colorSimilarity(50)->colorOverride(Color.YELLOW));
+    // e_trim:50:yellow
+
+    return [
+      'e_trim',
+      this._tolerance,
+      this._color
+    ].filter((a) => a).join(':');
+  }
+}
+
+
+export {TrimAction};
