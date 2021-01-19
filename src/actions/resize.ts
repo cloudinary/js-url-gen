ee56ec97b4dbc35890088aa2add4ffb264c90a92
Patrick <patrick.tolosa@cloudinary.com>
2021-01-19 10:00:32 +0200

    Fix JSDoc

--- src/actions/resize.ts
+++ src/actions/resize.ts
@@ -77,7 +77,7 @@ function crop(width?: number|string, height?: number|string) :ResizeCropAction {
  * @memberOf Actions.Resize
  * @param {number|string} width The required width of a transformed asset.
  * @param {number|string} height The required height of a transformed asset.
- * @return {ResizeAdvancedAction}
+ * @return {ResizeFillAction}
  */
 function fill(width?: string|number, height?: string|number) :ResizeFillAction {
   return new ResizeFillAction('fill', width, height);
