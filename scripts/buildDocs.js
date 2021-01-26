68564db1c8cca0e22f646b54813a01e118ce7de1
Patrick <patrick.tolosa@cloudinary.com>
2021-01-26 14:56:30 +0200

    Update the build:docs script to build our template library

--- scripts/buildDocs.js
+++ scripts/buildDocs.js
@@ -2,4 +2,4 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 
-execSync(`jsdoc -u ./__DOC_EXAMPLES__ --configure jsdoc.config.json --verbose --readme ./DOCS.README.md --destination public/docs/ && cp __DOC_RESOURCES__/customStyles.css public/docs/`, {stdio: 'inherit'})
+execSync(`(cd ./__docTemplate && grunt) && jsdoc -u ./__DOC_EXAMPLES__ --configure jsdoc.config.json --verbose --readme ./DOCS.README.md --destination public/docs/ && cp __DOC_RESOURCES__/customStyles.css public/docs/`, {stdio: 'inherit'})
