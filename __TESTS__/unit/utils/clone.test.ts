39557c3d75541da762d15b7a3eb870313322d898
Raya Straus <raya@cloudinary.com>
2021-01-31 17:37:47 +0200

    update test

--- __TESTS__/unit/utils/clone.test.ts
+++ __TESTS__/unit/utils/clone.test.ts
@@ -1,12 +1,20 @@
 import {CloudinaryMedia} from "../../../src/assets/CloudinaryMedia";
+import {Effect} from "../../../src/actions/effect";
 
 describe('Tests for cloning', () => {
   it('should clone resource', () => {
-    const url = new CloudinaryMedia('sample', {cloudName: 'demo'});
+    const asset = new CloudinaryMedia('sample', {cloudName: 'demo'});
 
-    const cloneUrl = url.clone();
+    const cloneAsset = asset.clone();
 
-    expect(cloneUrl).toEqual(url);
+    //add transformation to asset
+    asset.effect(Effect.sepia());
+
+    //change public id of cloneAsset
+    cloneAsset.setPublicID('dog');
+
+    expect(cloneAsset.toURL()).toEqual('https://res.cloudinary.com/demo/image/upload/dog');
+    expect(asset.toURL()).toEqual('https://res.cloudinary.com/demo/image/upload/e_sepia/sample');
   });
 });
 
