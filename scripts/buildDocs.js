b8dc4f3a1216c8ebe98027001670863261a97da7
Patrick <patrick.tolosa@cloudinary.com>
2021-02-08 12:29:11 +0200

    Docs - Add version number to the docs reference

--- scripts/buildDocs.js
+++ scripts/buildDocs.js
@@ -1,5 +1,19 @@
 /* eslint-disable */
 const { execSync } = require('child_process');
 const fs = require('fs');
+const pkg = require('../package.json');
 
-execSync(`(cd ./__docTemplate && grunt) && jsdoc -u ./__DOC_EXAMPLES__ --configure jsdoc.config.json --verbose --destination public/docs/ && cp __DOC_RESOURCES__/customStyles.css public/docs/`, {stdio: 'inherit'})
+// Inject the package json version to the Docs reference footer and navbar
+const data = `
+$('footer').append($('<div/>').html('SDK Version - <b>${pkg.version}</b>'));
+var content = $('.branding-logo').html();
+$('.branding-logo').html(content + ' - ${pkg.version}');
+`;
+fs.writeFileSync('__DOC_RESOURCES__/injectVersionSemver.js', data);
+
+execSync(`
+  (cd ./__docTemplate && grunt) && 
+  jsdoc -u ./__DOC_EXAMPLES__ --configure jsdoc.config.json --verbose --destination public/docs/ && 
+  cp __DOC_RESOURCES__/customStyles.css public/docs/ && 
+  cp __DOC_RESOURCES__/injectVersionSemver.js public/docs/
+`, {stdio: 'inherit'});
