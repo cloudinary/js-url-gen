293eb4d15d0aced20c2e02bb84a97e95d8fd5ad8
Patrick <patrick.tolosa@cloudinary.com>
2021-02-21 16:50:03 +0200

    Add Theme Effect

---
+++ src/actions/effect/theme.ts
@@ -0,0 +1,46 @@
+import {Qualifier} from "../../internal/qualifier/Qualifier";
+import {QualifierValue} from "../../internal/qualifier/QualifierValue";
+import {Action} from "../../internal/Action";
+import {SystemColors} from "../../qualifiers/color";
+
+/**
+ * @extend {SDK.Action}
+ * @memberOf {Actions.Effect}
+ * @description Changes the main background color to the one specified, as if a 'theme change' was applied (e.g. dark mode vs light mode).
+ */
+class ThemeEffect extends Action {
+  private color: SystemColors;
+  private _photosensitivity: number;
+  private effectName = 'theme';
+
+  constructor(color:SystemColors) {
+    super();
+    this.color = color;
+  }
+
+  /**
+   * @description The sensitivity to photographic elements of an image.
+   *              A value of 0 treats the whole image as non-photographic.
+   *              A value of 200 treats the whole image as photographic, so no theme change is applied.
+   * @param {number} photosensitivity
+   * @return {this}
+   */
+  photosensitivity(photosensitivity: number): this {
+    this._photosensitivity = photosensitivity;
+    return this;
+  }
+
+
+  protected prepareQualifiers(): void {
+    const sensitivity = this._photosensitivity ? `:photosensitivity_${this._photosensitivity}` : '';
+    // Replace # in hex colors (#fff -> fff)
+    const val = `${this.effectName}:color_${this.color.replace('#', '')}${sensitivity}`;
+
+    this.addQualifier(new Qualifier('e', new QualifierValue(val)));
+    return;
+  }
+}
+
+
+
+export {ThemeEffect};
