f7eeb18411681bd126bd2cd2973dbfc398238124
Patrick <patrick.tolosa@cloudinary.com>
2021-03-04 16:26:48 +0200

    Add examples and docstrings to Overlay and Underlay

--- src/actions/overlay.ts
+++ src/actions/overlay.ts
@@ -5,14 +5,33 @@ import {LayerAction} from "./layer/LayerAction";
  * @description Adds a video, text or an image layer as an overlay over the base layer. </br>
  * @memberOf Actions
  * @namespace Overlay
+ * @see Visit {@link Qualifiers.TextStyle|TextStyle} for advanced text options
+ * @see {@link Actions.Underlay| The underlay action}
  * @example
  * import {Cloudinary} from "@cloudinary/base/instance/Cloudinary";
  *
  * const yourCldInstance = new Cloudinary({cloud:{cloudName:'demo'}});
- * const image = yourCldInstance.image('woman');
+ * const myVideo = yourCldInstance.video('dog');
  *
- * image.overlay(
- *       Overlay.source(Source.image('sample'))
+ * import {source} from "@cloudinary/base/actions/overlay"
+ * import {image, video, text} from "@cloudinary/base/qualifiers/source"
+ * import {TextStyle} from '@cloudinary/base/qualifiers/textStyle
+ *
+ * myVideo.overlay(
+ *       source(image('myImage'))
+ *     )
+ *
+ * myVideo.overlay(
+ *       source(video('myVideo'))
+ *     )
+ *
+ * myVideo.overlay(
+ *       source(text('My text'), 'arial_15')
+ *     )
+ *
+ * // Or a text with more complex options
+ * myVideo.overlay(
+ *       source(text('My text'), new TextStyle('arial', 50))
  *     )
  */
 
