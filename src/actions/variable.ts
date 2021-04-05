8705417a8c3bc7b4f8c5ad1b61833ed3ac6a781a
Patrick <patrick.tolosa@cloudinary.com>
2021-04-05 15:16:49 +0300

    Improve variable docs

--- src/actions/variable.ts
+++ src/actions/variable.ts
@@ -8,7 +8,31 @@ import {ExpressionQualifier} from "../qualifiers/expression/ExpressionQualifier"
  * Defines a new user variable with the given value.
  * @memberOf Actions
  * @namespace Variable
+ * @example
+ * import {Cloudinary} from "@cloudinary/base/instance/Cloudinary";
+ * import {
+ *  set,
+ *  setAssetReference,
+ *  setFromContext,
+ *  setFromMetadata
+ * } from "@cloudinary/base/actions/variable";
+ * import {scale} from "@cloudinary/base/actions/resize";
+ *
+ * const yourCldInstance = new Cloudinary({cloud:{cloudName:'demo'}});
+ *
+ * const image1 = yourCldInstance.image('woman');
+ * image1.addVariable(set('foo', 100)).resize(scale().width('$foo'));
+ *
+ * const image2 = yourCldInstance.image('woman');
+ * image2.addVariable(setAssetReference('foo', 'val'));
+ *
+ * const image3 = yourCldInstance.image('woman');
+ * image3.addVariable(setFromContext('foo', 'val'));
+ *
+ * const image4 = yourCldInstance.image('woman');
+ * image4.addVariable(setFromMetadata('foo', 'val'));
  */
+
 
 /**
  * @summary action
