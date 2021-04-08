dc4f449d9ba1dbac1edf00e77070d80962a0d707
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2021-04-08 11:50:20 +0300

    Improve the docs for transcode (#374)

--- src/actions/transcode/VideoCodecAction.ts
+++ src/actions/transcode/VideoCodecAction.ts
@@ -5,6 +5,7 @@ import {AdvVideoCodecType, VideoCodecType} from "../../qualifiers/videoCodecType
  * @extends SDK.Action
  * @memberOf Actions.Transcode
  * @description Converts a video to an animated webp or gif.
+ * @see Visit {@link Actions.Transcode|Transcode} for an example
  */
 class VideoCodecAction extends Action {
   constructor(videoCodecTypeQualifier : VideoCodecType | AdvVideoCodecType) {
