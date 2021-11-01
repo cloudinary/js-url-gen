05d3621544b9e4a9c71bf8bfc0f9d496fc2d6aea
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2021-11-01 16:57:07 +0200

    Refactor fromJson tests (#468)

---
+++ __TESTS__/unit/fromJson/resize.fromJson.test.ts
@@ -0,0 +1,33 @@
+import {fromJson} from "../../../src/internal/fromJson";
+
+describe('resize.fromJson', () => {
+  it('should generate a url with resize actions from array of models', function () {
+    const transformation = fromJson([
+      {actionType: 'scale', dimensions: {width: 100, aspectRatio: 7}},
+      {actionType: 'fit', dimensions: {height: 200, aspectRatio: '16:9'}, relative: true},
+      {actionType: 'limitFit', dimensions: {height: 200, aspectRatio: 'ignore_aspect_ratio'}, relative: true},
+      {actionType: 'minimumFit', dimensions: {width: 100}, regionRelative: true},
+      {actionType: 'crop', dimensions: {width: 100}, regionRelative: true, gravity: 'north_east', y: 3, zoom: 7},
+      {actionType: 'fill', dimensions: {width: 100}, relative: true, gravity: 'south', x: 4, y: 5},
+      {actionType: 'limitFill', dimensions: {width: 100}, relative: true, gravity: 'south', x: 4, y: 5},
+      {actionType: 'thumbnail', dimensions: {width: 100}, relative: true, gravity: 'south', zoom: 4},
+      {actionType: 'pad', dimensions: {width: 100}, relative: true, gravity: 'south', x: 3, y:4, background: 'white'},
+      {actionType: 'limitPad', dimensions: {width: 100}, relative: true, gravity: 'south', x: 3, y:4, background: 'white'},
+      {actionType: 'minimumPad', dimensions: {width: 100}, relative: true, gravity: 'south', x: 3, y:4, background: 'white'},
+    ]);
+
+    expect(transformation.toString()).toStrictEqual([
+      'ar_7.0,c_scale,w_100',
+      'ar_16:9,c_fit,fl_relative,h_200',
+      'c_limit,fl_ignore_aspect_ratio,fl_relative,h_200',
+      'c_mfit,fl_region_relative,w_100',
+      'c_crop,fl_region_relative,g_north_east,w_100,y_3,z_7',
+      'c_fill,fl_relative,g_south,w_100,x_4,y_5',
+      'c_lfill,fl_relative,g_south,w_100,x_4,y_5',
+      'c_thumb,fl_relative,g_south,w_100,z_4',
+      'b_white,c_pad,fl_relative,g_south,w_100,x_3,y_4',
+      'b_white,c_lpad,fl_relative,g_south,w_100,x_3,y_4',
+      'b_white,c_mpad,fl_relative,g_south,w_100,x_3,y_4',
+    ].join('/'));
+  });
+});
