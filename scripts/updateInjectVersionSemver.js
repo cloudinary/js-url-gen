f27745da35505e20154f19ba524bfd504942aec3
maoznir <nir.maoz@cloudinary.com>
2022-02-01 11:46:01 +0200

    Add documentation to updateInjectVersionSemver.js

--- scripts/updateInjectVersionSemver.js
+++ scripts/updateInjectVersionSemver.js
@@ -1,4 +1,8 @@
 /* eslint-disable */
+/**
+ * This script updates the file __DOCS__/resources/injectVersionSemver.js.
+ * It needs to run on every docs generation so that we have an updated pacakge version in docs.
+ */
 const fs = require('fs');
 const pkg = require('../package.json');
 
