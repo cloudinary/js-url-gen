d97c3acd3b5bd0e9d1c831732686589eae045ced
Lior Ben-David <lior.ben-david@cloudinary.com>
2023-01-26 12:31:52 +0200

    increase allowed bundle size by 1 KB

--- __TESTS_BUNDLE_SIZE__/bundleSizeTestCases.ts
+++ __TESTS_BUNDLE_SIZE__/bundleSizeTestCases.ts
@@ -74,7 +74,7 @@ const bundleSizeTestCases:ITestCase[] = [
   },
   {
     name: 'Import all of the SDK',
-    sizeLimitInKB: 129,
+    sizeLimitInKB: 130,
     importsArray: [
       importFromPackage('* as CloudinaryURLGEN')
     ]
