c97e7bef3a7558295d00bc00ccbb92c8a4ef4aa5
Lior Ben-David <lior.ben-david@cloudinary.com>
2023-10-16 11:24:29 +0300

    fix broken test

--- __TESTS__/unit/analytics/analytics.browser.test.ts
+++ __TESTS__/unit/analytics/analytics.browser.test.ts
@@ -14,10 +14,10 @@ describe('Add analytics to a URL from the browser', () => {
       }
     });
 
-    // BATAAB{NODE_VERSION}0
-    // BATAAB{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
-    // expect BATAABAA0
-    expect(url).toContain('sample?_a=BATAABAA0'); // we shouldn't have a query param at all
+    // CATAABAAZ{NODE_VERSION}0
+    // CATAABAAZ{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
+    // expect CATAABAAZAA0
+    expect(url).toContain('sample?_a=CATAABAAZAA0'); // we shouldn't have a query param at all
   });
 
   it('Uses default techVersion 0.0.0 when in browser for image with file extension', () => {
@@ -28,10 +28,10 @@ describe('Add analytics to a URL from the browser', () => {
       }
     });
 
-    // BATAAB{NODE_VERSION}0
-    // BATAAB{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
-    // expect BATAABAA0
-    expect(url).toContain('sample.jpg?_a=BATAABAA0'); // we shouldn't have a query param at all
+    // CATAABAAZ{NODE_VERSION}0
+    // CATAABAAZ{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
+    // expect CATAABAAZAA0
+    expect(url).toContain('sample.jpg?_a=CATAABAAZAA0'); // we shouldn't have a query param at all
   });
 
   it('Uses default techVersion 0.0.0 when in browser for video', () => {
@@ -42,10 +42,10 @@ describe('Add analytics to a URL from the browser', () => {
       }
     });
 
-    // BATAAB{NODE_VERSION}0
-    // BATAAB{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
-    // expect BATAABAA0
-    expect(url).toContain('sample?_a=BATAABAA0'); // we shouldn't have a query param at all
+    // CATAABAAZ{NODE_VERSION}0
+    // CATAABAAZ{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
+    // expect CATAABAAZAA0
+    expect(url).toContain('sample?_a=CATAABAAZAA0'); // we shouldn't have a query param at all
   });
 
 
@@ -57,9 +57,9 @@ describe('Add analytics to a URL from the browser', () => {
       }
     });
 
-    // BATAAB{NODE_VERSION}0
-    // BATAAB{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
-    // expect BATAABAA0
-    expect(url).toContain('sample.webm?_a=BATAABAA0'); // we shouldn't have a query param at all
+    // CATAABAAZ{NODE_VERSION}0
+    // CATAABAAZ{AA}0 -> we expect nodeVersion to be 0.0.0 in browser (Since it's missing)
+    // expect CATAABAAZAA0
+    expect(url).toContain('sample.webm?_a=CATAABAAZAA0'); // we shouldn't have a query param at all
   });
 });
