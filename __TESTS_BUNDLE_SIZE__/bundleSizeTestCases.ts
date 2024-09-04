cae92ff7439eb9849af864a7e9f2734153b34c60
Lisa <yelyzaveta.kuzmina@cloudinary.com>
2024-09-04 16:09:00 +0200

    Increase all actions size limit in test

--- __TESTS_BUNDLE_SIZE__/bundleSizeTestCases.ts
+++ __TESTS_BUNDLE_SIZE__/bundleSizeTestCases.ts
@@ -88,7 +88,7 @@ const bundleSizeTestCases:ITestCase[] = [
   },
   {
     name: 'Import All Actions',
-    sizeLimitInKB: 64,
+    sizeLimitInKB: 65,
     importsArray: [
       importFromPackage('Actions')
     ]
